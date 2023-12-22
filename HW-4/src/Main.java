4ffe088de6e2e114bb2d3654bedd9466b00a4ff7
Смирнов Михаил <smirnov_mikhael@bk.ru>
2023-12-22 13:51:25 +0300

    Добавили автодокументацию и исправили код

--- HW-4/src/Main.java
+++ HW-4/src/Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
+/**
+ * Главный класс приложения, демонстрирующий взаимодействие счетов и транзакций.
+ */
 public class Main {
+
+    /**
+     * Точка входа в программу.
+     * @param args Аргументы командной строки (не используются).
+     */
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
